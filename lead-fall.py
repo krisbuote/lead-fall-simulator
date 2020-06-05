update Ltot calculation for finding kr1 and b. Rope model still needs work, not correct

lead-fall.py
@@ -19,13 +19,11 @@ Sdrag_y = 0.14
 Lslack = 0.3 # Slack at belay
 
 # Rope params
-# KR1 = 13900 # N . Need to divide by R before using. Where R = initial rope length from pro to climber including slack before stretch
-# B = 6768 # N*s
+KR1meter = 13900 # N . KR1 = KR1meter / Ltot. Where Ltot = L1 + L2 + Lslack
+Bmeter = 6768 # N*s BR1 = Bmeter / Ltot
 
-# KR1 = 1390
-# B = 676
 
-parameters = [Mc, g, ro, Sdrag_x, Sdrag_y, KR1, B, Lslack]
+parameters = [Mc, g, ro, Sdrag_x, Sdrag_y, KR1meter, Bmeter, Lslack]
 climber_init_pos = [0, 2]
 pro_pos = [0, 1]
 belay_pos = [0, 0]
@@ -36,7 +34,7 @@ numsteps = 1000
 # Fall factor = fall distance (in y) / length of rope out
 fall_factor = ((climber_init_pos[1] - pro_pos[1]) * 2 + Lslack) / (LA.norm(np.array(pro_pos)) + LA.norm(np.array(climber_init_pos) - np.array(pro_pos)) + Lslack)
 
-solver = ODESolver(init_position=climber_init_pos, pro_position=pro_pos, initial_velocity=init_vel, params=parameters, duration=simulation_duration, numsteps=numsteps)
+solver = ODESolver(init_position=climber_init_pos, pro_position=pro_pos, belay_position=belay_pos, initial_velocity=init_vel, params=parameters, duration=simulation_duration, numsteps=numsteps)
 solver.solve()
 
 df = solver.report() # Get dataframe of fall data
@@ -82,45 +80,45 @@ print("Fall factor: {0}".format(fall_factor))
 df.to_csv('runge-kutta-soln.csv')
 
 # ### Plot
-# df.plot(x="Time", y=["Frope_total", "Fdrag_total"])
+df.plot(x="Time", y=["Frope_total"])
 # df.plot(x="Time", y=["ux", "uy", "vx", "vy"])
-# plt.show()
-
-
-### Animate
-x1 = df['ux']
-y1 = df['uy']
-dt = simulation_duration / numsteps
-
-fig = plt.figure()
-ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 5), ylim=(-2, 5))
-ax.grid()
-
-line, = ax.plot([], [], 'o-', lw=2)
-time_template = 'time = %.1fs'
-time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
-Frope_template = 'Rope force = %.1fs'
-Frope_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
-
-
-def init():
-    line.set_data([], [])
-    time_text.set_text('')
-
-    return line, time_text
-
-
-def animate(i):
-  # anchor, current position
-    thisx = [pro_pos[0], x1[i]]
-    thisy = [pro_pos[1], y1[i]]
-
-    line.set_data(thisx, thisy)
-    time_text.set_text(time_template % (i*dt))
-    return line, time_text
-
-ani = animation.FuncAnimation(fig, animate, np.arange(1, len(x1)),
-                              interval=25, blit=True, init_func=init)
-
-# ani.save('double_pendulum.mp4', fps=15)
 plt.show()
+
+
+# ### Animate
+# x1 = df['ux']
+# y1 = df['uy']
+# dt = simulation_duration / numsteps
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 5), ylim=(-2, 5))
+# ax.grid()
+
+# line, = ax.plot([], [], 'o-', lw=2)
+# time_template = 'time = %.1fs'
+# time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+# Frope_template = 'Rope force = %.1fs'
+# Frope_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+
+
+# def init():
+#     line.set_data([], [])
+#     time_text.set_text('')
+
+#     return line, time_text
+
+
+# def animate(i):
+#   # anchor, current position
+#     thisx = [pro_pos[0], x1[i]]
+#     thisy = [pro_pos[1], y1[i]]
+
+#     line.set_data(thisx, thisy)
+#     time_text.set_text(time_template % (i*dt))
+#     return line, time_text
+
+# ani = animation.FuncAnimation(fig, animate, np.arange(1, len(x1)),
+#                               interval=25, blit=True, init_func=init)
+
+# # ani.save('double_pendulum.mp4', fps=15)
+# plt.show()

rungekutta.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 class ODESolver(object):
 
-	def __init__(self, init_position, pro_position, initial_velocity, params, duration, numsteps):
+	def __init__(self, init_position, pro_position, belay_position, initial_velocity, params, duration, numsteps):
 		# Initial climber position
 		self.ux = init_position[0]
 		self.uy = init_position[1]
@@ -22,8 +22,10 @@ class ODESolver(object):
 
 		# Rope parameters. Determine rope properties based on L2 + Lslack
 		self.R = np.sqrt((self.ux - self.Tx)**2 + (self.uy - self.Ty)**2) + self.Lslack # initial rope length from pro to climber including slack before stretch
-		self.kr1 = self.KR1 / self.R # Rope values from Sporri determined by amount of rope out from pro
-		self.b = self.B / self.R
+		self.belay_to_pro_dist = np.sqrt((self.Tx - belay_position[0])**2 + (self.Ty - belay_position[1])**2)
+		
+		self.kr1 = self.KR1 / (self.R + self.belay_to_pro_dist) # Rope values from Sporri determined by amount of rope out from pro
+		self.b = self.B / (self.R + self.belay_to_pro_dist)
 
 		self.t = 0
 		self.duration = duration